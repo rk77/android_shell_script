5510b455649247dd70c552edc3cfea5c54a477e9
gaofengx <xugaofeng@topscomm.com>
2020-01-09 09:52:40 +0800

    add random delay time to cycle swipe

--- cycle_swipe.sh
+++ cycle_swipe.sh
@@ -1,9 +1,22 @@
 #! /system/bin/sh
 i=0
+min=10
+max=10
+
+function random(){ 
+
+  num=$(date +%s+%N);
+  ((retnum=num%max+min));
+  echo $retnum;
+}
+
 while [ $i -le 1000 ]
 do
   echo "-----$i----------"
   let i++
   input swipe 500 800 500 0
-  sleep 20 
+  delay=$(random)
+  echo "delay time:$delay"
+  sleep $delay
 done
+
